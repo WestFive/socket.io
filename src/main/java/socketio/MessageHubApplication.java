8b87911863cdfa10ca98d5b0cb46145ddc3ed827
leechenlong <chenlong5469@163.com>
2018-06-08 09:45:50 +0800

    "add eruka client "

--- src/main/java/socketio/MessageHubApplication.java
+++ src/main/java/socketio/MessageHubApplication.java
@@ -6,13 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 //import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class MessageHubApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
